f9ec13ff416a290079cdfa9721149893ead37737
pashabetenya <pavelbiatsenia@gmail.com>
2023-02-20 03:59:43 +0300

    creating a project, and setting up the environment.

---
+++ MVC/application/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
